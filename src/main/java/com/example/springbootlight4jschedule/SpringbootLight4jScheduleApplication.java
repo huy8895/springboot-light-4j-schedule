9dc684a537c7699e03d95912c209fff8dfb5b5f6
huy8895 <huy8895@gmail.com>
2023-04-16 11:22:55 +0700

    Initial commit

---
+++ src/main/java/com/example/springbootlight4jschedule/SpringbootLight4jScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootlight4jschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLight4jScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLight4jScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootlight4jschedule/SpringbootLight4jScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlight4jschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLight4jScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
